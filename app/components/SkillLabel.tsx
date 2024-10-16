fdd50682c11d82a341556a462818d220725a8ba4
rchrisarevalo <rubenplayer105@gmail.com>
2024-10-16 01:49:40 -0500

    UI change

--- app/components/SkillLabel.tsx
+++ app/components/SkillLabel.tsx
@@ -6,7 +6,7 @@ interface SkillLabelProps {
 
 const SkillLabel: React.FC<SkillLabelProps> = ({ skillName }) => {
   return (
-    <span className="flex items-center w-[100px] h-[50px] rounded-xl text-center justify-center mr-10 bg-slate-500 text-white font-[800] border-2 border-solid border-slate-300 p-5">
+    <span className="flex items-center w-[100px] h-[50px] mb-5 rounded-xl text-center justify-center mr-10 bg-slate-500 text-white font-[800] border-2 border-solid border-slate-300 p-5 pl-7 pr-7">
       {skillName}
     </span>
   );
