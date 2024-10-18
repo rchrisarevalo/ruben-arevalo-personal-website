00e77495abe6f2c9474cc3405e535f2ae61c9e59
rchrisarevalo <rubenplayer105@gmail.com>
2024-10-18 14:26:31 -0500

    UI fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,8 +6,7 @@ const Footer = () => {
       <section className="ml-10 mr-10 flex flex-row max-sm:flex-col justify-between max-sm:space-y-5">
         <span className="flex">
           <p className="text-md">
-            <span className="max-sm:hidden">© 2024 Ruben Christopher Arevalo. All rights reserved.</span>
-            <span className="max-xl:hidden max-sm:block">© 2024 Ruben Christopher Arevalo. <br></br> All rights reserved.</span>
+            <span>© 2024 Ruben Christopher Arevalo. All rights reserved.</span>
           </p>
         </span>
         <span>
