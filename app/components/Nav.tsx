cd2eee35889e2c3de163bff8782cffcce99ab3d1
rchrisarevalo <rubenplayer105@gmail.com>
2024-11-02 18:51:00 -0500

    UI change to nav bar

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,10 +4,10 @@ import React from "react";
 const Nav = () => {
   return (
     <nav className="z-10 flex bg-black bg-opacity-75 items-center justify-between flex-row fixed top-0 w-screen p-7">
-      <span className="text-lg font-thin max-sm:hidden">
+      <span className="text-lg font-thin">
         <ul className="flex flex-row gap-2 cursor-pointer">
           <li>
-            <Link href="/" className="p-2 pl-6 pr-6 rounded-lg transition ease-in-out hover:bg-white hover:text-black font-[300] uppercase tracking-wider text-[20px]">Ruben Arevalo</Link>
+            <a href="/" className="p-1 rounded-lg transition ease-in-out hover:bg-white hover:text-black font-[300] uppercase tracking-wider text-[20px]">Ruben Arevalo</a>
           </li>
         </ul>
       </span>
